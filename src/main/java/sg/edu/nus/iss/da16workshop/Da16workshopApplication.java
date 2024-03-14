1fcc9b91111c6bf1c171d71cdf7d3554654f0bdb
simyt <e0036029@u.nus.edu>
2024-03-14 14:02:56 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/da16workshop/Da16workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.da16workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Da16workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Da16workshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/da16workshop/Da16workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.da16workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Da16workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
